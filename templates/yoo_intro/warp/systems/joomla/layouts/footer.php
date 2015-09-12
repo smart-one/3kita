55306e0c8774d0bd172f7369b7783dfc418a4b17
egolife <egor.zubarev@gmail.com>
2015-09-12 14:17:14 +0300

    move all files to public_html

--- templates/yoo_intro/warp/systems/joomla/layouts/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-* @package   Warp Theme Framework
-* @author    YOOtheme http://www.yootheme.com
-* @copyright Copyright (C) YOOtheme GmbH
-* @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
-*/
-    //footer content
-?>
